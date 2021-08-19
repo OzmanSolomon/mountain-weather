05905275028888a189f0562889b12b07c6b400e5
Osman Solomon <39956261+OzmanSolomon@users.noreply.github.com>
2021-08-19 10:40:22 +0400

    Initial Commit

---
+++ Mountain Weather/Mountain_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mountain_WeatherApp.swift
+//  Mountain Weather
+//
+//  Created by Osman Solomon on 19/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Mountain_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
